ce1c1c7bba6c78fd47d488d7f37423383c782845
dtr200 <negevski@yandex.ru>
2021-05-27 00:10:32 +0300

    home page has been updated

--- src/components/pages/home-page.js
+++ src/components/pages/home-page.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Quiz from '../quiz';
 
 const HomePage = () => {
     return (
-        <h1>Home Page</h1>
+        <main className="page-main">
+            <h1 className="page-main__title">Home Page</h1>
+            <Quiz />
+        </main>
     )
 }
 
---
+++ src/components/quiz/quiz.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './quiz.css';
+
+const Quiz = () => {
+    return (
+        <input type='text' />
+    )
+}
+
+export default Quiz;
