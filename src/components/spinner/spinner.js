37301ded45762d43d40a416ed0dec4bc345f631b
dtr200 <negevski@yandex.ru>
2021-05-31 23:55:03 +0300

    spinner corrected

--- src/components/spinner/spinner.js
+++ src/components/spinner/spinner.js
@@ -4,8 +4,8 @@ import './spinner.css';
 
 const Spinner = () => {
     return (
-        <div class="loadingio-spinner-spinner-88ct2rgsrdg spinner">
-            <div class="ldio-yp881gue0zp">
+        <div className="loadingio-spinner-spinner-88ct2rgsrdg spinner">
+            <div className="ldio-yp881gue0zp">
                 <div></div>
                 <div></div>
                 <div></div>
